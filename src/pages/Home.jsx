631ef302c5c861b960cb83c898ba421c8b9698b4
DarkVortex <112894912+Jaun-van-Deventer@users.noreply.github.com>
2024-11-09 17:26:16 +0200

    Added buttons to profiles
    Added buttons

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import '../App.css'
 
 function Home(){
     // State to track which half the cursor is hovering over
@@ -10,16 +11,17 @@ function Home(){
       <div className="home-container">
         {/* Left Half */}
         <div
-          className={`half left-half ${hoveredHalf === 'left' ? 'focused' : 'blurred'}`}
+          className={`half left-half ${hoveredHalf === 'left' ? 'focused' : 'blurred'} `}
           onMouseEnter={() => setHoveredHalf('left')}
           onMouseLeave={() => setHoveredHalf(null)}
         >
-          <Link to="/jaun" >
-          <div className="content, link-no-underline"  style={{ textDecoration: 'none', color: 'white' }}>
+          <div className="content"  style={{ color: 'white' }}>
             <h1>Welcome to My Resume</h1>
             <p>About me, skills, and more...</p>
+            <Link to="/jaun" >
+            <button type="button" class="btn btn-light">Click here to learn more</button>
+            </Link>
           </div>
-          </Link>
         </div>
   
         {/* Right Half */}
@@ -28,9 +30,12 @@ function Home(){
           onMouseEnter={() => setHoveredHalf('right')}
           onMouseLeave={() => setHoveredHalf(null)}
         >
-          <div className="content">
+          <div className="content" style={{ color: 'white' }}>
             <h1>Contact Information</h1>
             <p>Let's connect!</p>
+            <Link to="/chevonne" >
+            <button type="button" class="btn btn-light">Click here to learn more</button>
+            </Link>
           </div>
         </div>
       </div>
